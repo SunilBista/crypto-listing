e0fb71f21f75c156646d220b2aa90adddd49d3bd
SunilBista <sunilbista303@gmail.com>
2025-08-07 13:29:00 +1000

    feat: update app and design

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Container, AppBar, Toolbar, Typography } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Table } from "./components/Table";
+import { Detail } from "./components/Detail";
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              align="center"
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              Crypto Market Tracker
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+          <Routes>
+            <Route path="/" element={<Table />} />
+            <Route path="/crypto/:id" element={<Detail />} />
+          </Routes>
+        </Container>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;

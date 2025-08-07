d44fa2d569f4b28d75d1d4af7d24540d1797c01d
SunilBista <sunilbista303@gmail.com>
2025-08-07 20:10:29 +1000

    misc: add type for crypto table and detail data

---
+++ src/types/crypto.ts
@@ -0,0 +1,59 @@
+export interface CryptoCurrencyType {
+  id: string;
+  symbol: string;
+  name: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  ath: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+  image: string;
+  fully_diluted_valuation: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  last_updated: string;
+}
+
+export type CryptoTableType = {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  fully_diluted_valuation: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number | null;
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  roi: {
+    times: number;
+    currency: string;
+    percentage: number;
+  } | null;
+  last_updated: string;
+};
